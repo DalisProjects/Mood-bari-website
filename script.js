c827d7acd32aca05e772f60ee4953f19f9673b47
Mohamed Ali MHADHBI <dali1.mhedhbi@gmail.com>
2026-02-14 18:59:57 +0100

    Update demo with obfuscated JS and minified CSS/HTML

--- script.js
+++ script.js
@@ -1,35 +1,3 @@
-function scrollToSection(){
-  document.getElementById("contact").scrollIntoView({
-    behavior:"smooth"
-  });
-}
-
-const track = document.querySelector('.carousel-track');
-const slides = Array.from(track.children);
-const prevButton = document.querySelector('.prev');
-const nextButton = document.querySelector('.next');
-let index = 0;
-
-function updateCarousel() {
-  const slideWidth = slides[0].getBoundingClientRect().width + 10; // include margin
-  track.style.transform = `translateX(-${index * slideWidth}px)`;
-}
-
-nextButton.addEventListener('click', () => {
-  index = (index + 1) % slides.length;
-  updateCarousel();
-});
-
-prevButton.addEventListener('click', () => {
-  index = (index - 1 + slides.length) % slides.length;
-  updateCarousel();
-});
-
-// Auto-slide every 3 seconds
-setInterval(() => {
-  index = (index + 1) % slides.length;
-  updateCarousel();
-}, 6000);
-
-// Update carousel on resize
-window.addEventListener('resize', updateCarousel);
+function scrollToSection(){document.getElementById("contact").scrollIntoView({behavior:"smooth"})}
+const track=document.querySelector('.carousel-track');const slides=Array.from(track.children);const prevButton=document.querySelector('.prev');const nextButton=document.querySelector('.next');let index=0;function updateCarousel(){const slideWidth=slides[0].getBoundingClientRect().width+10;track.style.transform=`translateX(-${index * slideWidth}px)`}
+nextButton.addEventListener('click',()=>{index=(index+1)%slides.length;updateCarousel()});prevButton.addEventListener('click',()=>{index=(index-1+slides.length)%slides.length;updateCarousel()});setInterval(()=>{index=(index+1)%slides.length;updateCarousel()},6000);window.addEventListener('resize',updateCarousel)
