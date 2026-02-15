40aa8261306859df68b25e960d379da241d1dda9
Mohamed Ali MHADHBI <dali1.mhedhbi@gmail.com>
2026-02-15 23:17:27 +0100

    website update

--- Old/script.js
+++ Old/script.js
@@ -30,6 +30,11 @@ setInterval(() => {
   index = (index + 1) % slides.length;
   updateCarousel();
 }, 1800);
+const toggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
 
+toggle.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
 // Update carousel on resize
 window.addEventListener('resize', updateCarousel);
